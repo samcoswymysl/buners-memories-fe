033de9db661c0c26cb435615066f3ca7dc7d06b4
samcoswymysl <rafalkrukowskipraca123@gmail.com>
2022-07-06 21:01:20 +0200

    added Recat redux & post reducers & store & actions

---
+++ src/reducers/posts.js
@@ -0,0 +1,11 @@
+export const posts = (state = [], action) => {
+    switch (action.type) {
+        case 'FETCH_ALL':
+            return action.payload
+        case 'CREATE_POST':
+            return [...state, action.payload]
+        default:
+            return state
+
+    }
+}
