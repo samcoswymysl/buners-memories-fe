293a98a8262a43c8e4c92b6dd52575287cbb0d99
samcoswymysl <rafalkrukowskipraca123@gmail.com>
2022-07-12 18:21:33 +0200

    Added methods to connect with API

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,13 @@
-import axios from 'axios';
+import axios from "axios"
 
-const url = 'http://localhost:5000/posts'
+const url = "https://buners-memories-app.herokuapp.com/posts"
 
 export const fetchPost = async () => await axios.get(url)
 
-export const createPost =async (newPost) => await axios.post(url, newPost)
+export const createPost = async (newPost) => await axios.post(url, newPost)
+
+export const updatePost = async (id, updatePost) => await axios.patch(`${url}/${id}`, updatePost)
+
+export const deletePost = async (id) => await axios.delete(`${url}/${id}`)
+
+export const likePost = async (id) => await axios.patch(`${url}/${id}/like`)
