e40f3e4ba55082736f307b812d7afe9506c7eed8
samcoswymysl <rafalkrukowskipraca123@gmail.com>
2022-07-12 18:22:14 +0200

    Added Posts component

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,25 +1,31 @@
-import React from 'react'
-import {useSelector} from 'react-redux'
+import React from "react"
+import { useSelector } from "react-redux"
+import { Grid, CircularProgress } from "@material-ui/core"
 
-import {Post} from "./Post/Post";
+import { Post } from "./Post/Post"
 
+import useStyles from "./styles"
 
+export const Posts = ({ setCurrId }) => {
+  const posts = useSelector((state) => state.posts)
 
-import useStyles from './styles'
+  console.log(posts)
 
-export const Posts = ()=> {
-
-    const posts = useSelector((state)=> state.posts)
-
-    console.log(posts)
-
-    const classes = useStyles()
-    return(
-        <>
-            <h2>POSTS</h2>
-            <Post/>
-            <Post/>
-            <Post/>
-        </>
-    )
+  const classes = useStyles()
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} setCurrId={setCurrId} />
+        </Grid>
+      ))}
+    </Grid>
+  )
 }
