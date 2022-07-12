c967280ec9f1ea9954542910ef5f02346a3d9607
samcoswymysl <rafalkrukowskipraca123@gmail.com>
2022-07-12 18:21:03 +0200

    Added Actions and reducers

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,21 +1,51 @@
-import * as api from '../api'
+import {CREATE_POST, DELETE, FETCH_ALL, LIKE, UPDATE} from '../constants/actionTypes'
+import * as api from "../api"
 
 export const getPosts = () => async (dispatch) => {
-    try{
-        const {data} = await api.fetchPost()
-        dispatch({type: 'FETCH_ALL', payload: data})
+  try {
+    const { data } = await api.fetchPost()
+    dispatch({ type: FETCH_ALL, payload: data })
+  } catch (e) {
+    console.log(e.message)
+  }
+}
 
-    } catch (e) {
-        console.log(e.message)
-    }}
+export const createPost = (post) => async (dispatch) => {
+  try {
+    const { data } = await api.createPost(post)
 
+    dispatch({ type: CREATE_POST, payload: data })
+  } catch (e) {
+    console.log(e)
+  }
+}
 
-export const createPost = (post) => async (dispatch)=>{
-    try {
-        const {data} = await api.createPost(post)
+export const updatePost = (id, updatePost) => async (dispatch) => {
+  try {
+    const { data } = await api.updatePost(id, updatePost)
 
-        dispatch({type: 'CREATE_POST', payload: data})
-    } catch (e) {
-        console.log(e)
-    }
+    dispatch({ type: UPDATE, payload: data })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    await api.deletePost(id)
+    dispatch({ type: DELETE, payload: id })
+  } catch (e) {
+    console.log(e.message)
+  }
+}
+
+export const likePost = (id) => async (dispatch) => {
+  try {
+    const { data } = await api.likePost(id)
+    console.log(data)
+
+    dispatch({ type: LIKE, payload: data })
+  } catch (e) {
+    console.log(e)
+  }
 }
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,11 +1,19 @@
-export const posts = (state = [], action) => {
-    switch (action.type) {
-        case 'FETCH_ALL':
-            return action.payload
-        case 'CREATE_POST':
-            return [...state, action.payload]
-        default:
-            return state
+import {CREATE_POST, DELETE, FETCH_ALL, LIKE, UPDATE} from "../constants/actionTypes";
 
-    }
+export const posts = (state = [], action) => {
+  switch (action.type) {
+    case DELETE:
+      return state.filter((post) => post._id !== action.payload)
+    case UPDATE:
+    case LIKE:
+      return state.map((post) => {
+        return post._id === action.payload._id ? action.payload : post
+      })
+    case FETCH_ALL:
+      return action.payload
+    case CREATE_POST:
+      return [...state, action.payload]
+    default:
+      return state
+  }
 }
